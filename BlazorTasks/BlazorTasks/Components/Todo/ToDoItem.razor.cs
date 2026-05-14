fa1edf8f3ba6d982a6fbedb38143700893502dfe
Palchamy, Akshaya <akshaya@m-s.in>
2026-05-14 15:19:13 +0530

    CHANGED: Added ToDoItem component UI

---
+++ BlazorTasks/BlazorTasks/Components/Todo/ToDoItem.razor.cs
@@ -0,0 +1,17 @@
+﻿using BlazorTasks.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorTasks.Components
+{
+    public partial class ToDoItem: ComponentBase 
+    {
+        [Parameter]
+        public TodoItem? TodoTask { get; set; }
+
+        [Parameter]
+        public EventCallback<int> OnDone { get; set; }
+
+        [Parameter]
+        public EventCallback<int> OnDelete { get; set; }
+    }
+}
