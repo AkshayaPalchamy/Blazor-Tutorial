f2a4cfae9aa586fd7c9c488f29c44e9a4c56d7e3
Palchamy, Akshaya <akshaya@m-s.in>
2026-05-14 12:49:14 +0530

    ADDED: ToDoItem model

---
+++ BlazorTasks/BlazorTasks/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorTasks.Models
+{
+    public class TodoItem
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
